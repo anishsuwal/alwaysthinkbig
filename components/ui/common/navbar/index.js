9fa3f225d86daaa11e2dbe748651589fb35314b2
DELL <anishbhaktasuwal@gmail.com>
2022-05-19 16:42:08 +0545

    5 commit

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -52,7 +52,7 @@ const Navbar = () => {
         {
           Links.map((link)=>(
             <li key={link.name} className='md:ml-8 -400 text-sm md:my-0 my-7'>
-              <a href={link.link} onClick={() => setOpen=false} className='text-white hover:text-gray-400 duration-500'>{link.name}</a>
+              <a href={link.link} className='text-white hover:text-gray-400 duration-500'>{link.name}</a>
             </li>
           ))
         }
